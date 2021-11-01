a78024c3b76c947cf681076711a7307472623b10
silverback23 <cahid23@hotmail.com>
2021-11-01 15:10:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler= new string [5];
+            string [] hayvanlar={"kedi","köpek","kuş"};
+
+            int [] dizi;
+            dizi=new int[5];
+
+
+            //dizilere değer atama ve erişim
+
+            renkler [0]= "Mavi";
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(renkler[3]);
+
+            //Döngülerle Dizi
+
+            Console.Write("Lütfen dizinin eleman saisini giriniz");
+            int diziuzunlugu=int.Parse(Console.ReadLine());
+
+            int [] sayiDizisi=new int[diziuzunlugu];
+
+            for (int i = 0; i < diziuzunlugu; i++)
+            {
+                Console.Write("lütfen {0}.sayisi giriniz",i+1);
+                sayiDizisi[i]=int.Parse(Console.ReadLine());
+
+            }
+            
+            int toplam =0;
+            
+            foreach (var sayi in sayiDizisi)
+                toplam +=sayi;
+            
+            Console.Write("ortalama :"+toplam/diziuzunlugu);
+                
+           
+
+
+
+
+
+        }
+    }
+}
